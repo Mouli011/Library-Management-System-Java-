a0415b23bf6692a4bf575ea965ceb98c7890ba4b
Mouli011 <98518598+Mouli011@users.noreply.github.com>
2022-07-23 17:31:52 +0530

    Update Resources.java

--- src/librarymanagementsystem/Resources.java
+++ src/librarymanagementsystem/Resources.java
@@ -17,27 +17,6 @@ public class Resources {
     public static HashSet<Book> lostBooks = new HashSet<Book>();
     
     public static int lostBookCount = 0;
+    public static int totalBooksOnHoldByAllPatrons=0;
     
-    public static void displayLostBooks()
-    {
-        if(lostBookCount!=0)
-        {
-        System.out.println("--------------------------------------------------------------------------------------------------------------------------------------");  
-        System.out.printf("%5s %20s %15s %20s %20s %20s", "BOOK ID", "BOOK NAME","BOOK NO",  "AUTHOR", "PUBLISHED IN", "GENRE");  
-        System.out.println();  
-        System.out.println("--------------------------------------------------------------------------------------------------------------------------------------");  
-             //System.out.println("Book ID\t\tBook Name\t\tBook No\t\tAuthor\t\tPublisher\t\tAvailability");
-             for(Book bookCopy:lostBooks)
-             {
-             System.out.printf("%5s %20s %15s %20s %20s %22s", bookCopy.bookID, bookCopy.bookName,bookCopy.bookNo, bookCopy.author, bookCopy.publishedIn, bookCopy.genre);  
-            System.out.println();  
-        System.out.println("--------------------------------------------------------------------------------------------------------------------------------------");  
-             }
-         }
-        else
-        {
-            System.out.println("No Books Lost!!");
-        }
-    
-    }
 }
