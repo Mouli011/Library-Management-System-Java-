ce824efd65070f99d534c8235fea267cbdaeb259
Mouli011 <98518598+Mouli011@users.noreply.github.com>
2022-07-27 21:56:54 +0530

    Add files via upload

---
+++ librarymanagementsystem/BOOKS/BookPopularityComparator.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package librarymanagementsystem;
+import java.util.*;
+/**
+ *
+ * @author mouli011
+ */
+public class BookPopularityComparator implements Comparator<Book> {
+
+    @Override
+    public int compare(Book book1, Book book2) {
+        return book2.borrowedCopies - book1.borrowedCopies;
+    }
+
+}
